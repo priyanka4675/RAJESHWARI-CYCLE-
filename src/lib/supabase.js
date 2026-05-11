4dc64accc02a7e4c17cb8dd7a69bb5dfd482d1fe
priyanka4675 <priyanka.pilla999@gmail.com>
2026-05-11 22:21:53 +0530

    supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://gdrojabvlkdqmxrxobkb.supabase.co";
+
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdkcm9qYWJ2bGtkcW14cnhvYmtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NzkyNjUsImV4cCI6MjA5NDA1NTI2NX0.loIlNuazEQirAjvrW0hR4badLMCQpOxu7-YS37kVoy0";
 
 export const supabase = createClient(
   supabaseUrl,
