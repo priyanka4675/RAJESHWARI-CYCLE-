3c9a423ab785f8ca0ef10295bc7dc344b9039330
priyanka4675 <priyanka.pilla999@gmail.com>
2026-05-12 13:14:48 +0530

    supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,11 +2,12 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://gdrojabvlkdqmxrxobkb.supabase.co";
 
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdkcm9qYWJ2bGtkcW14cnhvYmtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NzkyNjUsImV4cCI6MjA5NDA1NTI2NX0.loIlNuazEQirAjvrW0hR4badLMCQpOxu7-YS37kVoy0";
+const supabaseAnonKey =
+  "YOUR_ANON_KEY";
 
-export const supabase = createClient(
+const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
 );
 
-console.log("SUPABASE =", supabase);
+export default supabase;
